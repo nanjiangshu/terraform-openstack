43dd8c478ea6284cc5854274571d5e8b0164ff5e
nanjiangshu <nanjiang.shu@gmail.com>
2022-05-19 21:38:49 +0200

    feat: install docker compose CLI

--- recipes/docker-ubuntu/src/install_docker.sh
+++ recipes/docker-ubuntu/src/install_docker.sh
@@ -12,4 +12,8 @@ if [ ! -f   /usr/local/bin/docker-compose ];then
     curl -L https://github.com/docker/compose/releases/download/1.26.2/docker-compose-`uname -s`-`uname -m` > /tmp/docker-compose; chmod +x /tmp/docker-compose; sudo cp /tmp/docker-compose /usr/local/bin/docker-compose
 fi
 
+# install docker compose CLI
+mkdir -p ~/.docker/cli-plugins/
+curl -SL https://github.com/docker/compose/releases/download/v2.2.2/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose && chmod +x ~/.docker/cli-plugins/docker-compose
+
 exit 0
