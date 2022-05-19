41ec832f13c17e7e941625bf5803f0f51325baf5
nanjiangshu <nanjiang.shu@gmail.com>
2022-05-19 22:35:50 +0200

    feat: update the default ubuntu version

--- recipes/chemical-space-ubuntu/variables.tf
+++ recipes/chemical-space-ubuntu/variables.tf
@@ -33,7 +33,7 @@ variable public_ssh_key {
 
 variable image_name {
     description = "Name of the image for the instance"
-    default =  "Ubuntu 18.04 LTS (Bionic Beaver) - latest"
+    default =  "Ubuntu 20.04 LTS (Focal Fossa) - latest"
 }
 variable flavor_name {
     description = "Name of the image for the instance"
