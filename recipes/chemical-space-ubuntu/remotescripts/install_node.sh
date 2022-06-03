beaa6f2e69e3cbfd01fe0228da810ec3aff393e3
nanjiangshu <nanjiang.shu@gmail.com>
2022-06-03 16:58:33 +0200

    fix: bashrc interactive issue

--- recipes/chemical-space-ubuntu/remotescripts/install_node.sh
+++ recipes/chemical-space-ubuntu/remotescripts/install_node.sh
@@ -3,6 +3,7 @@
 
 cd /tmp
 wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
-. ~/.bashrc
+grep -v return ~/.bashrc > ~/.sourcefile
+. ~/.sourcefile
 nvm install node 
 exit 0
